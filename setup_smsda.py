85b6726a8411d0d65b1572293fa0163793a411d5
Billy Arante <arantebillywilson@gmail.com>
2020-03-31 09:16:44 +0800

    add: setup_smsda.py

---
+++ setup_smsda.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# setup.py
+# Loads all necessary resources for product development of SMSDA.
+#
+# Billy Arante <arantebillywilson@gmail.com>
+# March 31, 2020
+
+import webbrowser
+import os
+
+def main():
+    # Notes
+    webbrowser.open('https://docs.google.com/document/d/1dFzUfaxnbzKsrsyQ2CEq2zpT0-o5HlzHeTXK8pI_QMg/edit#heading=h.u65jai413vty')
+
+    # Documentation
+    webbrowser.open('https://docs.google.com/document/d/1ch2rQPegf52Bx8ESlJkv9beKWMAxh1lrWH4CPnsYD1c/edit')
+
+    # Web app for admin
+    webbrowser.open_new('http://localhost:8000');
+
+    # Web app for regular users
+    os.system('/usr/bin/google-chrome -incognito http://localhost:8000')
+
+
+if __name__ == '__main__':
+    main()
