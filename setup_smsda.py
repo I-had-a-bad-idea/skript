fdd0064e983cda91dba1cd26d55c03617e3eff5b
Billy Wilson Arante <arantebillywilson@gmail.com>
2020-05-06 17:42:17 +0800

    update: setup_smsda.py as of May 6, 2020

--- setup_smsda.py
+++ setup_smsda.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # setup.py
 # Loads all necessary resources for product development of SMSDA.
@@ -13,8 +13,8 @@ def main():
 	# xampp
     os.system('sudo /opt/lampp/lampp start')
 
-    # notes
-    webbrowser.open('https://docs.google.com/document/d/1dFzUfaxnbzKsrsyQ2CEq2zpT0-o5HlzHeTXK8pI_QMg/edit#heading=h.u65jai413vty')
+    # github project
+    webbrowser.open('https://github.com/arantebw/project-may-lynn/projects/1?fullscreen=true')
 
     # documentation
     webbrowser.open('https://docs.google.com/document/d/1ch2rQPegf52Bx8ESlJkv9beKWMAxh1lrWH4CPnsYD1c/edit')
@@ -30,13 +30,13 @@ def main():
 
     # tmux
     os.system('tmux new-session -d -s \'SMSDA\'')
-    os.system('tmux send-keys \'cd src/\' C-m')
+    os.system('tmux send-keys \'cd project-may-lynn/\' C-m')
     os.system('tmux split-window -h')
-    os.system('tmux send-keys \'cd src/ && npm run watch-poll --scripts-prepend-node-path\' C-m')
+    os.system('tmux send-keys \'cd project-may-lynn/ && npm run watch-poll --scripts-prepend-node-path\' C-m')
     os.system('tmux split-window -v')
-    os.system('tmux send-keys \'cd src/ && php artisan serve\' C-m')
+    os.system('tmux send-keys \'cd project-may-lynn/ && php artisan serve\' C-m')
     os.system('tmux new-window')
-    os.system('tmux send-keys \'cd src/ && vim\' C-m')
+    os.system('tmux send-keys \'cd project-may-lynn/ && vim .\' C-m')
     os.system('tmux -2 attach-session -d')
 
 
