012f9c0ce0507a8482c37b928cedff4a7143c89a
Billy Arante <arantebillywilson@gmail.com>
2022-11-23 07:41:30 +0800

    feat(alliance_setup.py): add the alliance_setup.py file

---
+++ alliance_setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# alliance_setup.py
+#
+# Author: Billy Arante <arantebillywilson@gmail.com>
+# Created: 23 Sep 2022
+# Updated: 23 Sep 2022
+#
+# How to use:
+# python alliance_setup.py <path>
+
+import os
+import sys
+
+
+def change_path(new_path):
+    os.system(f"tmux send-keys 'cd {new_path}' C-m")
+
+
+def main():
+    project_path = sys.argv[1]
+
+    os.system("tmux new-session -d -s 'alliance'")
+
+    change_path(project_path)
+    os.system("tmux send-keys 'clear' C-m")
+
+    os.system("tmux split-window -h")
+    change_path(project_path)
+    os.system("tmux send-keys 'clear' C-m")
+
+    os.system("tmux split-window -v")
+    change_path(project_path)
+    os.system("tmux send-keys 'clear' C-m")
+
+    os.system("tmux select-pane -L")
+    os.system("tmux split-window -v")
+    change_path(project_path)
+    os.system("tmux send-keys 'clear' C-m")
+
+    os.system("tmux rename-window -t 0 'servers'")
+
+    os.system("tmux -2 attach-session -d")
+
+    os.system("tmux send-keys 'kitty @ set-window-title alliance' C-m")
+
+
+if __name__ == "__main__":
+    main()
