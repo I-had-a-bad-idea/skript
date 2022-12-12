715dcfb0aca74de1c4eac817fe5251314749b46e
Billy Arante <arantebillywilson@gmail.com>
2022-12-12 15:36:46 +0800

    feat(install_vins.py): add description

--- install_vins.py
+++ install_vins.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# install_vins.py
+# Download and intall all of the vim plugins indicated in the vins.csv.
+#
+# How to execute:
+# python install_vins.py ~/.dotfiles/vins.csv
+#
+# TODO: In coc.nvim, switch to the release branch after installation.
+
 import sys
 import os
 
@@ -16,7 +24,7 @@ def get_repos(file):
 def main(file):
     repos_list = get_repos(file)
 
-    vins_dir = '~/.vim/pack/third-party/opt/'
+    vins_dir = '~/.vim/pack/third-party/start/'
     for repo in repos_list:
         ssh_uri = repo.split(',')[0]
         repo_dir = vins_dir + repo.split(',')[1]
